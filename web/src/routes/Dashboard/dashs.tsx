f8dffdd94907f36eecaea9840f9c13a95874a08b
ferried <harlancui@outlook.com>
2020-12-09 10:22:53 +0800

    :bug: (fix all charts): fix all charts to use g2plot

--- web/src/routes/Dashboard/dashs.tsx
+++ web/src/routes/Dashboard/dashs.tsx
@@ -1,36 +1,55 @@
 import { Col, Card } from "antd";
 import moment from "moment";
 import React from "react";
-import { YzAreaChart, YzLongAreaChart } from "simple-g2-charts";
+import { Area } from "@ant-design/charts";
 
 const RedisMemoryUsedBytes: React.FC<any> = ({ getValue }) => {
   return (
     <Card title="redis_memory_used_bytes">
-      <YzLongAreaChart
-        id="redis_memory_used_bytes"
-        data={getValue("redis_memory_used_bytes")}
-        xindex="time"
-        autoFit={true}
+      <Area
         height={300}
-        scales={[
-          {
-            index: "time",
-            type: "time",
-            alias: "时间",
-            tickCount: 20,
-            mask: "MM/DD HH:mm:ss",
+        autoFit={true}
+        data={getValue("redis_memory_used_bytes")}
+        xAxis={{
+          title: {
+            text: "日期",
           },
-          {
-            index: "value",
-            alias: "使用大小",
-            color1: "#6600CC",
-            color2: "#33CCFF",
+          label: {
             formatter: (v) => {
-              return (v / 1024 / 1024).toFixed(2) + "MB";
+              return moment(parseFloat(v) * 1000).format("YYYY-MM-DD hh时 ");
             },
           },
-        ]}
-      ></YzLongAreaChart>
+        }}
+        yAxis={{
+          min: 0,
+          title: {
+            text: "使用大小",
+          },
+          label: {
+            formatter: (v) => {
+              return (parseInt(v) / 1024 / 1024).toFixed(2) + "MB";
+            },
+          },
+        }}
+        xField="time"
+        yField="value"
+        scrollbar={true}
+        slider={{
+          start: 0,
+          end: 1,
+          formatter: (v) => {
+            return moment(v * 1000).format("YYYY-MM-DD hh时");
+          },
+        }}
+        tooltip={{
+          showCrosshairs: true,
+          follow: true,
+          shared: true,
+          crosshairs: {
+            type: "xy",
+          },
+        }}
+      ></Area>
     </Card>
   );
 };
@@ -38,31 +57,50 @@ const RedisMemoryUsedBytes: React.FC<any> = ({ getValue }) => {
 const RedisCpuUserSecondsTotal: React.FC<any> = ({ getValue }) => {
   return (
     <Card title="redis_cpu_user_seconds_total">
-      <YzLongAreaChart
-        id="redis_cpu_user_seconds_total"
-        data={getValue("redis_cpu_user_seconds_total")}
-        xindex="time"
-        autoFit={true}
+      <Area
         height={300}
-        scales={[
-          {
-            index: "time",
-            type: "time",
-            alias: "时间",
-            tickCount: 20,
-            mask: "MM/DD HH:mm:ss",
+        autoFit={true}
+        data={getValue("redis_cpu_user_seconds_total")}
+        xAxis={{
+          title: {
+            text: "日期",
           },
-          {
-            index: "value",
-            alias: "占用时长",
-            color1: "#FF3300",
-            color2: "#FFFF33",
+          label: {
             formatter: (v) => {
-              return v + "毫秒";
+              return moment(parseFloat(v) * 1000).format("YYYY-MM-DD hh时 ");
             },
           },
-        ]}
-      ></YzLongAreaChart>
+        }}
+        yAxis={{
+          min: 0,
+          title: {
+            text: "使用大小",
+          },
+          label: {
+            formatter: (v) => {
+              return `${v}毫秒`;
+            },
+          },
+        }}
+        xField="time"
+        yField="value"
+        scrollbar={true}
+        slider={{
+          start: 0,
+          end: 1,
+          formatter: (v) => {
+            return moment(v * 1000).format("YYYY-MM-DD hh时");
+          },
+        }}
+        tooltip={{
+          showCrosshairs: true,
+          follow: true,
+          shared: true,
+          crosshairs: {
+            type: "xy",
+          },
+        }}
+      ></Area>
     </Card>
   );
 };
@@ -70,32 +108,50 @@ const RedisCpuUserSecondsTotal: React.FC<any> = ({ getValue }) => {
 const RedisMemoryUsedLuaBytes: React.FC<any> = ({ getValue }) => {
   return (
     <Card title="redis_memory_used_lua_bytes">
-      <YzLongAreaChart
-        id="redis_memory_used_lua_bytes"
-        data={getValue("redis_memory_used_lua_bytes")}
-        xindex="time"
-        autoFit={true}
+      <Area
         height={300}
-        scales={[
-          {
-            index: "time",
-            type: "time",
-            alias: "时间",
-            tickCount: 20,
-            mask: "MM/DD HH:mm:ss",
+        autoFit={true}
+        data={getValue("redis_memory_used_lua_bytes")}
+        xAxis={{
+          title: {
+            text: "日期",
           },
-          {
-            index: "value",
-            min: 0,
-            alias: "使用大小",
-            color1: "#6600CC",
-            color2: "#33CCFF",
+          label: {
             formatter: (v) => {
-              return (v / 1024 / 1024).toFixed(2) + "MB";
+              return moment(parseFloat(v) * 1000).format("YYYY-MM-DD hh时 ");
             },
           },
-        ]}
-      ></YzLongAreaChart>
+        }}
+        yAxis={{
+          min: 0,
+          title: {
+            text: "使用大小",
+          },
+          label: {
+            formatter: (v) => {
+              return (parseInt(v) / 1024 / 1024).toFixed(2) + "MB";
+            },
+          },
+        }}
+        xField="time"
+        yField="value"
+        scrollbar={true}
+        slider={{
+          start: 0,
+          end: 1,
+          formatter: (v) => {
+            return moment(v * 1000).format("YYYY-MM-DD hh时");
+          },
+        }}
+        tooltip={{
+          showCrosshairs: true,
+          follow: true,
+          shared: true,
+          crosshairs: {
+            type: "xy",
+          },
+        }}
+      ></Area>
     </Card>
   );
 };
@@ -104,23 +160,46 @@ const RedisDbKeys: React.FC<any> = ({ getValue }) => {
   console.log(getValue("redis_db_keys"));
   return (
     <Card title="redis_db_keys">
-      <YzAreaChart
-        id="redis_db_keys"
+      <Area
         height={300}
         autoFit={true}
         data={getValue("redis_db_keys")}
-        xname="时间"
-        xindex="time"
-        yname="数量"
-        yindex="value"
-        colorExp="db"
-        xformat={(text) => {
-          const d = new Date(parseFloat(text) * 1000);
-          const m = moment(d).format("YYYY-MM-DD HH:mm");
-          console.log(m)
-          return m;
+        xAxis={{
+          title: {
+            text: "日期",
+          },
+          label: {
+            formatter: (v) => {
+              return moment(parseFloat(v) * 1000).format("YYYY-MM-DD hh:mm:ss");
+            },
+          },
         }}
-      />
+        yAxis={{
+          min: 0,
+          title: {
+            text: "key/value数量",
+          },
+        }}
+        xField="time"
+        yField="value"
+        seriesField="db"
+        scrollbar={true}
+        slider={{
+          start: 0,
+          end: 1,
+          formatter: (v) => {
+            return moment(v * 1000).format("YYYY-MM-DD hh:mm:ss");
+          },
+        }}
+        tooltip={{
+          showCrosshairs: true,
+          follow: true,
+          shared: true,
+          crosshairs: {
+            type: "xy", // 展示十字辅助线
+          },
+        }}
+      ></Area>
     </Card>
   );
 };
